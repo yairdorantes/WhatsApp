ab0c8f90861e89ee1bd3b008fb8fbc14afa3fc73
yair <yairmasterlol@gmail.com>
2023-01-22 14:23:09 -0600

    ready

--- myapp/src/controllers/WS.js
+++ myapp/src/controllers/WS.js
@@ -1,9 +1,13 @@
 import randomEmoji from "random-unicode-emoji";
+
 import client from "../index.js";
 
 const sendMessage = async (req, res) => {
-  client.sendMessage("5217293737947@c.us", randomEmoji.random({ count: 1 })[0]);
+  client.sendMessage("5217293737947@c.us", "HELLO");
+
   res.json(randomEmoji.random({ count: 1 })[0]);
+
+  // res.json("jaja");
 };
 
 export const wp = { sendMessage };
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,55 +1,53 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import { Client } from "whatsapp-web.js";
-const client = new Client();
 import qrcode from "qrcode-terminal";
 import randomEmoji from "random-unicode-emoji";
 
 dotenv.config();
 
-async function main() {
-  const port = process.env.PORT || 8000;
-  console.log(port);
-  try {
-    // const mongoose = require("mongoose");
-    app.listen(port, () => {
-      console.log("Server is running on port", port);
-    });
-  } catch (e) {
-    console.log(e);
-  }
+const client = new Client();
 
-  client.on("qr", (qr) => {
-    qrcode.generate(qr, { small: true });
+const port = process.env.PORT || 8000;
+console.log(port);
+try {
+  // const mongoose = require("mongoose");
+  app.listen(port, () => {
+    console.log("Server is running on port", port);
   });
-
-  client.on("ready", () => {
-    console.log("Client is ready!");
-  });
-
-  let intervalo;
-  let intervalRuning = false;
-  client.on("message", (message) => {
-    console.log(message.from);
-    console.log(message.body);
-    if (message.body === "Stop") {
-      clearInterval(intervalo);
-      client.sendMessage(message.from, "stopping service...");
-      intervalRuning = false;
-    } else {
-      if (intervalRuning === false) {
-        client.sendMessage(message.from, "ACTIVATING SERVICE...");
-        intervalo = setInterval(() => {
-          client.sendMessage(message.from, randomEmoji.random({ count: 1 })[0]);
-          client.sendMessage(message.from, "Write *Stop* to stop service");
-        }, 1000);
-        intervalRuning = true;
-      }
-    }
-    // ss
-  });
-  client.initialize();
+} catch (e) {
+  console.log(e);
 }
 
-main();
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+let intervalo;
+let intervalRuning = false;
+client.on("message", (message) => {
+  console.log(message.from);
+  console.log(message.body);
+  if (message.body === "Stop") {
+    clearInterval(intervalo);
+    client.sendMessage(message.from, "stopping service...");
+    intervalRuning = false;
+  } else {
+    if (intervalRuning === false) {
+      client.sendMessage(message.from, "ACTIVATING SERVICE...");
+      intervalo = setInterval(() => {
+        client.sendMessage(message.from, randomEmoji.random({ count: 1 })[0]);
+        client.sendMessage(message.from, "Write *Stop* to stop service");
+      }, 1000);
+      intervalRuning = true;
+    }
+  }
+  // ss
+});
+client.initialize();
+
 export default client;
