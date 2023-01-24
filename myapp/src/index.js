544df0f2d80c192396bfdaad02dbad9984f8eae3
yair <yairmasterlol@gmail.com>
2023-01-24 14:58:37 -0600

    xd

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,12 +1,14 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-import { Client } from "whatsapp-web.js";
+// import { Client, MessageMedia } from "whatsapp-web.js";
+import pkg from "whatsapp-web.js";
+const { Client, MessageMedia } = pkg;
 import qrcode from "qrcode-terminal";
 // import randomEmoji from "random-unicode-emoji";
 import schedule from "node-schedule";
-const port = process.env.PORT || 8000;
+import moment from "moment-timezone";
+const port = process.env.PORT || 7000;
 dotenv.config();
-
 // const client = new Client();
 const client = new Client({
   puppeteer: {
@@ -16,6 +18,9 @@ const client = new Client({
 });
 
 console.log(port);
+moment.tz.setDefault("America/Mexico_City");
+
+let myMessage;
 try {
   // const mongoose = require("mongoose");
   app.listen(port, () => {
@@ -35,9 +40,43 @@ client.on("qr", (qr) => {
 client.on("ready", () => {
   console.log("Client is ready!");
 });
+client.on("message_reaction", (reaction) => {
+  console.log(reaction);
 
+  console.log(myMessage, "myid");
+  if (reaction.msgId.id === myMessage) {
+    console.log("reacciono a mensaje");
+    if (reaction.reaction === "👍") yo.delete(true);
+  }
+});
+
+let yo;
+let brandon = 0;
 client.on("message", (message) => {
-  // ss
+  console.log(message);
+  if (message.from.includes("g")) {
+    console.log("");
+  } else {
+    const ms = client.sendMessage(
+      message.from,
+      "Hola 👋, soy un bot 🤖, *Yair* te respondera en un momento 😃, Reacciona a este mensaje 👍 para eliminarme"
+    );
+    ms.then((msg) => {
+      myMessage = msg.id.id;
+      yo = msg;
+    });
+  }
+
+  if (message.from === "5217293255577-1621863748@g.us") {
+    if (message.author === "5217293629531@c.us") {
+      brandon += 1;
+      client.sendMessage(
+        message.from,
+        `mensajes enviados por Brandon: *${brandon}*`
+      );
+    }
+  }
+  // const my
 });
 client.initialize();
 
